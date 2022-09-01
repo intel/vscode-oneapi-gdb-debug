cae922bc1964ca0949393833892041fe335507f6
AKurkina <anastasiia.kurkina@intel.com>
2022-09-01 15:05:57 +0100

    Revert "DEV1A-4506 feat: new view for SIMD lanes + upd linter (#81)" (#83)
    This reverts commit f7122f88a5cdd626af7db86834143df768562c7f.

--- src/SimdProvider.ts
+++ src/SimdProvider.ts
@@ -5,29 +5,33 @@
  * SPDX-License-Identifier: MIT
  */
 
-"use strict";
-import * as vscode from "vscode";
-import { DebugProtocol } from "vscode-debugprotocol";
-import { SIMDViewProvider } from "./viewProviders/SIMDViewProvider";
+'use strict';
+import * as vscode from 'vscode';
+import { DebugProtocol } from 'vscode-debugprotocol';
 
 interface ThreadInfo {
     index: number;
-    threadId: number;
+    threadId: number; 
     name: string;
 }
 
 export class SimdProvider {
 
+    private view: SimdViewProvider;
     private threadsInfoArray: ThreadInfo[] = [];
 
     constructor(
-         private context: vscode.ExtensionContext,
-         private viewProvider: SIMDViewProvider
+        private context: vscode.ExtensionContext
     ) {
 
-        context.subscriptions.push(vscode.debug.registerDebugAdapterTrackerFactory("cppdbg", new SimdDebugAdapterProviderFactory(this)));
+        //Setup Webview
+        this.view = new SimdViewProvider(context.extensionUri);
+        context.subscriptions.push(
+            vscode.window.registerWebviewViewProvider("intelOneAPI.debug.simdview", this.view));
 
-        context.subscriptions.push(vscode.commands.registerCommand("intelOneAPI.debug.fetchSIMDInfo", async() => {
+        context.subscriptions.push(vscode.debug.registerDebugAdapterTrackerFactory('cppdbg', new SimdDebugAdapterProviderFactory(this)));
+
+        context.subscriptions.push(vscode.commands.registerCommand('intelOneAPI.debug.fetchSIMDInfo', async () => {
             this.fetchEMaskForAll();
             return;
 
@@ -36,92 +40,106 @@ export class SimdProvider {
         //We need to test if multi debug sessions get effected by this, we might need to initialize multiple instances of this object :/
         context.subscriptions.push(vscode.debug.onDidTerminateDebugSession(session => {
             console.log("Debug Session " + session.id + " terminated");
-            this.viewProvider.cleanView();
+            this.view.updateWith("");
         }));
 
         context.subscriptions.push(vscode.debug.onDidChangeBreakpoints(e => {
             console.log(e);
         }));
     }
+
     set threadsInfo(value: ThreadInfo[]){
         this.threadsInfoArray = value;
-    }
+      }
+      
+      get threadsInfo():ThreadInfo[]{
+         return this.threadsInfoArray;
+      }
 
-    get threadsInfo():ThreadInfo[]{
-        return this.threadsInfoArray;
+    public updateWithSimdData(s: SimdStatusData): void {
+
+        let others = "";
+        s.otherLanes.forEach(i => {
+            others = others.concat(i + "<br>");
+        });
+        this.view.updateWith("<h3>Active: " + s.activeLane + "</h3><br>" + others);
     }
 
     private matchIeMasktoSIMD(i: string): number {
-        switch (i.toLowerCase()) {
-        case "0xff": {
-            return 8;
-        }
-        case "0xffff": {
-            return 16;
-        }
-        case "0xffffffff": {
-            return 32;
-        }
+        switch(i.toLowerCase()) {
+            case "0xff": {
+                return 8;
+            }
+            case "0xffff": {
+                return 16;
+            }
+            case "0xffffffff": {
+                return 32;
+            }
         }
         //Did not match against known types
         return 0;
+
     }
 
     public async fetchEMaskForAll(): Promise<void> {
         const session = vscode.debug.activeDebugSession;
-
         if (session) {
             const r = await session.customRequest("threads");
 
             const threads = r.threads as DebugProtocol.Thread[];
-
-            await session.customRequest("evaluate", { expression: "-exec info threads", context: "repl" });
-
+          
+            await session.customRequest('evaluate', {expression: '-exec info threads', context: 'repl'});
+            
             const masks: emask[] = []; //optimise?
             let i = 0;
 
             for (const t of threads) {
-                this.viewProvider.setLoadingView();
                 const strarg: DebugProtocol.StackTraceArguments = { threadId: t.id };
-                const sTrace = await session.customRequest("stackTrace", strarg);
-                let evalargs: DebugProtocol.EvaluateArguments = { expression: "$emask", context: "repl", frameId: sTrace.stackFrames[0].id, format: { hex: true } };
-                let evalresult = await session.customRequest("evaluate", evalargs);
-
-                if (evalresult.result === "void" ) {
+                const sTrace = await session.customRequest('stackTrace', strarg);
+                let evalargs: DebugProtocol.EvaluateArguments = { expression: "$emask", context: "repl", frameId: sTrace.stackFrames[0].id, format: {hex: true}};
+                let evalresult = await session.customRequest('evaluate', evalargs);
+                if (evalresult.result === 'void' ) {
                     continue; //Filter Threads without EMASK
                 }
+                this.view.updateWith("waiting for data");
                 const maskvar = parseInt(evalresult.result, 16);
 
-                evalargs = { expression: "$iemask", context: "repl", frameId: sTrace.stackFrames[0].id,format: { hex: true } };
-                evalresult = await session.customRequest("evaluate", evalargs);
+                evalargs = { expression: "$iemask", context: "repl", frameId: sTrace.stackFrames[0].id,format: {hex: true} };
+                evalresult = await session.customRequest('evaluate', evalargs);
                 let maskLength = 0;
-
-                if (evalresult.result !== "void" ) {
+                if (evalresult.result !== 'void' ) {
                     maskLength = this.matchIeMasktoSIMD(evalresult.result as string);
                 }
 
-                evalargs = { expression: "$_thread", context: "repl", frameId: sTrace.stackFrames[0].id };
-                evalresult = await session.customRequest("evaluate", evalargs);
+                evalargs = { expression: "$_thread", context: "repl", frameId: sTrace.stackFrames[0].id};
+                evalresult = await session.customRequest('evaluate', evalargs);
                 let tid = 0;
-
-                if (evalresult.result !== "void" ) {
+                if (evalresult.result !== 'void' ) {
                     tid = parseInt(evalresult.result);
                 }
 
-                masks.push({ name: this.threadsInfoArray[i]?.name || t.name, threadId: tid, value: maskvar, length: maskLength });
+                masks.push({name: this.threadsInfoArray[i]?.name || t.name, threadId: tid, value: maskvar, length: maskLength});
 
                 i++;
             }
             if (!masks.length) {
                 return; //exit, no simd detected
             }
+            let upd = "<table><tbody><tr><td>ThreadID</td><td>Name</td><td>SIMD Lanes</td></tr>";
+            for (const m of masks) {
+                upd = upd + `<tr><td>${m.threadId}</td><td>${m.name}</td><td><code>${m.value.toString(2).padStart(m.length, "0")}</code></td></tr>`;
+            }
+            upd = upd + "</tbody></table>";
 
-            this.viewProvider.setView(masks);
+            this.view.updateWith(upd);
+
         }
     }
 }
 
-export interface emask {
+
+interface emask {
     name: string;
     threadId: number;
     value: number;
@@ -143,25 +161,37 @@ class SimdDebugAdapterProviderFactory implements vscode.DebugAdapterTrackerFacto
 
 class SimdDebugAdapterProvider implements vscode.DebugAdapterTracker {
 
-    constructor(private readonly simdtracker: SimdProvider) {}
+
+    constructor(
+        private readonly simdtracker: SimdProvider,
+    ) {
+
+
+    }
+
 
     public onDidSendMessage(m: DebugProtocol.ProtocolMessage) {
+
         this.routeDebugMessage(m);
     }
 
     private routeDebugMessage(m: DebugProtocol.ProtocolMessage): void {
         if (m.type === "event") {
             const e = m as DebugProtocol.Event;
-
             switch (e.event) {
-            case "stopped": {
-                // eslint-disable-next-line @typescript-eslint/no-unused-vars
-                const stopped = e as DebugProtocol.StoppedEvent;
+                case "stopped": {
+                    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+                    const stopped = e as DebugProtocol.StoppedEvent;
+                    this.simdtracker.fetchEMaskForAll();
+                }
 
-                this.simdtracker.fetchEMaskForAll();
-            }}}
+            }
+
+        }
         return;
-    }}
+    }
+
+}
 
 class SimdStatusData {
     constructor(
@@ -169,3 +199,51 @@ class SimdStatusData {
         public readonly otherLanes: string[],
     ) { }
 }
+
+class SimdViewProvider implements vscode.WebviewViewProvider {
+
+
+    private _view?: vscode.WebviewView;
+
+    constructor(
+        private readonly _extensionUri: vscode.Uri,
+    ) { }
+
+    public updateWith(data: string) {
+
+        if (this._view) {
+            this._view.webview.html = data;
+        }
+    }
+
+    public resolveWebviewView(
+        webviewView: vscode.WebviewView,
+        // context: vscode.WebviewViewResolveContext,
+        // _token: vscode.CancellationToken,
+    ) {
+        this._view = webviewView;
+
+        webviewView.webview.options = {
+            // Allow scripts in the webview
+            enableScripts: true,
+
+
+            localResourceRoots: [
+                this._extensionUri
+            ]
+        };
+
+        webviewView.webview.html = "";
+
+        webviewView.webview.onDidReceiveMessage(data => {
+            switch (data.type) {
+                case 'colorSelected':
+                    {
+                        vscode.window.activeTextEditor?.insertSnippet(new vscode.SnippetString(`#${data.value}`));
+                        break;
+                    }
+            }
+        });
+    }
+}
+
--- src/extension.ts
+++ src/extension.ts
@@ -4,42 +4,39 @@
  *
  * SPDX-License-Identifier: MIT
  */
-"use strict";
-import * as vscode from "vscode";
-import { LaunchConfigurator } from "./LaunchConfigurator";
-import { DebuggerCommandsPanel, getWebviewOptions, UserHelp } from "./UserHelp";
-import { SimdProvider } from "./SimdProvider";
-import { SIMDViewProvider } from "./viewProviders/SIMDViewProvider";
+ 'use strict';
+import * as vscode from 'vscode';
+import { LaunchConfigurator } from './LaunchConfigurator';
+import { DebuggerCommandsPanel, getWebviewOptions, UserHelp } from './UserHelp';
+import { SimdProvider } from './SimdProvider';
 
 function checkExtensionsConflict() {
     // The function of generating a launcher configuration from an deprecated extension conflicts with the same function from the current one.
-    const deprecatedExtension = vscode.extensions.getExtension("intel-corporation.oneapi-launch-configurator");
-    const actualExtension = vscode.extensions.getExtension("intel-corporation.oneapi-analysis-configurator"); // if only the deprecated version is installed, otherwise the new version will solve this problem and no action is required.
+    const deprecatedExtension = vscode.extensions.getExtension('intel-corporation.oneapi-launch-configurator');
+    const actualExtension = vscode.extensions.getExtension('intel-corporation.oneapi-analysis-configurator');
 
+    // if only the deprecated version is installed, otherwise the new version will solve this problem and no action is required.
     if (actualExtension === undefined && deprecatedExtension !== undefined) {
         if (deprecatedExtension) {
-            const Update = "Update";
+            const Update = 'Update';
             const deprExtName = deprecatedExtension.packageJSON.displayName;
-
-            vscode.window.showInformationMessage(`${deprExtName} is a deprecated version. This may lead to the unavailability of overlapping functions.`, Update, "Ignore")
+            vscode.window.showInformationMessage(`${deprExtName} is a deprecated version. This may lead to the unavailability of overlapping functions.`, Update, 'Ignore')
                 .then((selection) => {
                     if (selection === Update) {
-                        vscode.commands.executeCommand("workbench.extensions.uninstallExtension", deprecatedExtension.id).then(function() {
+                        vscode.commands.executeCommand('workbench.extensions.uninstallExtension', deprecatedExtension.id).then(function () {
                             vscode.window.showErrorMessage(`Completed uninstalling ${deprExtName} extension.`);
-                            vscode.commands.executeCommand("workbench.extensions.installExtension", "intel-corporation.oneapi-analysis-configurator").then(function() {
-                                const actualExtension = vscode.extensions.getExtension("intel-corporation.oneapi-analysis-configurator");
-
+                            vscode.commands.executeCommand('workbench.extensions.installExtension', 'intel-corporation.oneapi-analysis-configurator').then(function () {
+                                const actualExtension = vscode.extensions.getExtension('intel-corporation.oneapi-analysis-configurator');
                                 if (actualExtension) {
-                                    const Reload = "Reload";
-
-                                    vscode.window.showInformationMessage("Extension update completed. Please reload Visual Studio Code.", Reload)
+                                    const Reload = 'Reload';
+                                    vscode.window.showInformationMessage(`Extension update completed. Please reload Visual Studio Code.`, Reload)
                                         .then((selection) => {
                                             if (selection === Reload) {
-                                                vscode.commands.executeCommand("workbench.action.reloadWindow");
+                                                vscode.commands.executeCommand('workbench.action.reloadWindow');
                                             }
                                         });
                                 } else {
-                                    vscode.window.showErrorMessage("Extension could not be installed.");
+                                    vscode.window.showErrorMessage(`Extension could not be installed.`);
                                 }
                             });
                         });
@@ -54,58 +51,45 @@ export function activate(context: vscode.ExtensionContext): void {
     // Checking for outdated versions of extensions in the VS Code environment
     checkExtensionsConflict();
 
-    if (process.platform !== "linux") {
-        vscode.window.showWarningMessage("The Windows and macOS operating systems are not currently supported by the \"GDB GPU Support for Intel® oneAPI Toolkits\" extension. Debugging remote Linux systems from a Windows and macOS host is supported when using the various Microsoft \"Remote\" extensions.");
+    if (process.platform !== 'linux') {
+        vscode.window.showWarningMessage(`The Windows and macOS operating systems are not currently supported by the "GDB GPU Support for Intel® oneAPI Toolkits" extension. Debugging remote Linux systems from a Windows and macOS host is supported when using the various Microsoft "Remote" extensions.`);
     }
 
-    const provider = new SIMDViewProvider(context.extensionUri);
-    const simdViewDisposable = vscode.window.registerWebviewViewProvider(
-        SIMDViewProvider.viewType,
-        provider
-    );
-
-    context.subscriptions.push(simdViewDisposable);
-
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const simd = new SimdProvider(context, provider);
+    const simd = new SimdProvider(context);
     // Register the commands that will interact with the user and write the launcher scripts.
 
     const launchConfigurator = new LaunchConfigurator();
-
     launchConfigurator.checkGdb();
     if (vscode.workspace.workspaceFolders) {
         launchConfigurator.checkLaunchConfig();
     }
-    context.subscriptions.push(vscode.commands.registerCommand("intelOneAPI.launchConfigurator.generateLaunchJson", () => launchConfigurator.makeLaunchFile()));
+    context.subscriptions.push(vscode.commands.registerCommand('intelOneAPI.launchConfigurator.generateLaunchJson', () => launchConfigurator.makeLaunchFile()));
 
     // Register commands that will let user search through documentation easily
     const userHelp = new UserHelp();
-
-    context.subscriptions.push(vscode.commands.registerCommand("intelOneAPI.userHelp.openOnlineDocumentation", () => userHelp.openOnlineDocumentation()));
+    context.subscriptions.push(vscode.commands.registerCommand('intelOneAPI.userHelp.openOnlineDocumentation', () => userHelp.openOnlineDocumentation()));
 
     context.subscriptions.push(
-        vscode.commands.registerCommand("intelOneAPI.userHelp.displayDebuggerCommands", () => {
+        vscode.commands.registerCommand('intelOneAPI.userHelp.displayDebuggerCommands', () => {
             DebuggerCommandsPanel.createOrShow(context.extensionUri);
         })
     );
 
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const session = vscode.debug.activeDebugSession;
-
-    vscode.debug.registerDebugAdapterTrackerFactory("*", {
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        createDebugAdapterTracker(session) {
-            return {
+    vscode.debug.registerDebugAdapterTrackerFactory('*', {
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars
+            createDebugAdapterTracker(session) {
+              return {
                 onDidSendMessage: m => {
-                    if (JSON.stringify(m, undefined, 2).indexOf("Target Id") !== -1){
+                    if(JSON.stringify(m, undefined, 2).indexOf('Target Id') !== -1){
                         const body = m.body.output;
                         const threadsNumber = (body).match(/[+-]?([0-9]*[.])?[0-9]:\[[0-9]-[0-9]]/g);
                         const threadsResult = [];
-
                         for (const prop in threadsNumber) {
                             let second;
-
-                            if (+prop === threadsNumber.length-1) {
+                            if(+prop === threadsNumber.length-1) {
                                 second = body.length;
                             } else {
                                 second = body.indexOf(threadsNumber[+prop+1]);
@@ -113,9 +97,9 @@ export function activate(context: vscode.ExtensionContext): void {
                             const first = body.indexOf(threadsNumber[prop]);
                             const threadInfo = body.substring(first, second);
                             const nameTemplate = threadInfo.match(/(?<=Thread)(.*)(?=main::)/g)?.[0];
-                            const firstQuotes = nameTemplate.indexOf("\"");
-                            const lastQuotes = nameTemplate.lastIndexOf("\"");
-                            const name = ( firstQuotes !== -1 && lastQuotes !== -1 ) ? nameTemplate.substring(firstQuotes + 1, lastQuotes) : "";
+                            const firstQuotes = nameTemplate.indexOf('"');
+                            const lastQuotes = nameTemplate.lastIndexOf('"');
+                            const name = ( firstQuotes !== -1 && lastQuotes !== -1 ) ? nameTemplate.substring(firstQuotes + 1, lastQuotes) : '';
 
                             threadsResult.push({
                                 index: +prop,
@@ -126,9 +110,9 @@ export function activate(context: vscode.ExtensionContext): void {
                         simd.threadsInfo = threadsResult;
                     }
                 }
-            };
-        }
-    });
+              };
+            }
+          });
 
     if (vscode.window.registerWebviewPanelSerializer) {
         // Make sure we register a serializer in activation event
@@ -142,15 +126,13 @@ export function activate(context: vscode.ExtensionContext): void {
         });
     }
 
-    const tsExtension = vscode.extensions.getExtension("ms-vscode.cpptools");
-
+    const tsExtension = vscode.extensions.getExtension('ms-vscode.cpptools');
     if (!tsExtension) {
-        const GoToInstall = "Install C/C++ Extension";
-
-        vscode.window.showInformationMessage("No extension for C/C++ was found. Please install it to run Intel oneAPI launch configurations.", GoToInstall)
+        const GoToInstall = 'Install C/C++ Extension';
+        vscode.window.showInformationMessage('No extension for C/C++ was found. Please install it to run Intel oneAPI launch configurations.', GoToInstall)
             .then((selection) => {
                 if (selection === GoToInstall) {
-                    vscode.commands.executeCommand("workbench.extensions.installExtension", "ms-vscode.cpptools");
+                    vscode.commands.executeCommand('workbench.extensions.installExtension', 'ms-vscode.cpptools');
                 }
             });
     }
