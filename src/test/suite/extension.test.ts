242f0229659cc8234098abc86a3b29da0d4f69f9
Sergey Bobko <sergey.bobko@intel.com>
2022-11-22 13:48:03 +0100

    Develop to main for 2023.0 release (#110)
    * feat: new SIMD view + linter (#85)
    
    * fix: exec command (#86)
    
    * Device windows backend (#88)
    
    * Added backend for devicesWindow
    
    Co-authored-by: AKurkiina <anastasiia.kurkina@intel.com>
    
    * feat DEV1A-4507: add front for devices (#89)
    
    * feat: add front for devices
    * feat: group devices by thread groups
    
    Co-authored-by: Sergey <sergey.bobko@intel.com>
    
    * Dev1A-4584 improve performance simd (#90)
    
    * Added backend for devicesWindow
    
    * Refactored SIMD-panel backend
    
    * fix: refresh file paths (#94)
    
    fix: update loaded styles
    
    * feat: front end contect swich (#92)
    
    feat: context switch
    
    Co-authored-by: Sergey <sergey.bobko@intel.com>
    
    * fix: types (#97)
    
    * DEV1A-4586 update current thread (#98)
    
    fix: show current thread
    
    * DEV1A-4572 init ui test (#95)
    
    * Added notifications tests
    
    * Updated yarn.lock
    
    * fix: ui changes (#99)
    
    * feat: extend simd info (#100)
    
    * Upd version (#102)
    
    * fix: move docs panel to top (#103)
    
    * fix: move docs panel to top
    
    * fix: format docs (#104)
    
    * feat: upd version and changelog (#106)
    
    * fix: docs tables (#108)
    
    * feat: merge views (#109)
    
    * feat: merge views
    
    * Removed ChangeView buttom
    
    Co-authored-by: Sergey Bobko <sergey.bobko@intel.com>
    
    Co-authored-by: AKurkina <anastasiia.kurkina@intel.com>

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,11 +1,11 @@
-import * as assert from 'assert';
-import { after } from 'mocha';
+import * as assert from "assert";
+import { after } from "mocha";
 
 import * as vscode from 'vscode';
 import * as intellauncher from '../../extension';
 
-suite('Extension Test Suite', () => {
-    vscode.window.showInformationMessage('Extension test started!');
+suite("Extension Test Suite", () => {
+    vscode.window.showInformationMessage("Extension test started!");
 
 	test('Sample test', () => {
 		assert.equal([1, 2, 3].indexOf(5), -1);
