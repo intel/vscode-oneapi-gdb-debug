e7b3f8654bd540803bf2170cb7acc134d2d2e351
AKurkina <anastasiia.kurkina@intel.com>
2022-12-16 12:18:59 +0000

    feat: bugs readme upd (#116)
    feat: bugs and readme update

--- media/main.js
+++ media/main.js
@@ -31,14 +31,14 @@
 
         for (i = 0; i < coll.length; i++) {
             coll[i].addEventListener("click", function() {
-                this.classList.toggle("active");
                 var content = this.nextElementSibling;
-
-                if (content.style.display === "block") {
-                    content.style.display = "none";
+                
+                if (this.classList.contains('active')) {
+                  content.style.display = "none";
                 } else {
-                    content.style.display = "block";
+                  content.style.display = "block";
                 }
+                this.classList.toggle("active");
             });
         }
     }
