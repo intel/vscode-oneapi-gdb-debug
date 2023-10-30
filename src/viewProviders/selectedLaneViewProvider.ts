328189a3d62ecfccb4186306a711b915ecb6e7e9
Sergey Bobko <sergey.bobko@intel.com>
2023-10-30 07:55:29 +0100

    2024.0 Release | Develop to main (#166)
    * update simd panel (#148)
    
    * Added work group to GPU Threads panel
    
    * Add memView feat info (#149)
    
    * Add memView feat info
    
    * Add thread:lane BP validation (#151)
    
    * Added thread:lane BP input validation
    
    * chore: update packages
    
    * feat: integrate "install from notifications" test events
    
    * test: add "Generate 'Debug' launch configuration"
    
    * chore: clean yarn.lock file
    
    * feat: add UninstallExtension teardown function
    
    * tests: add 'help' related tests
    
    * refactor: add another replace
    
    * Removed wrapLocation prop (#150)
    
    * SIMD tooltip (#154)
    
    * Added SIMD tooltip
    
    * IGDB-4052 Replace "Location" content with File:Line in ONEAPI GPU THREADS View Panel (#157)
    
    * Replaced "Location" content with File:Line
    
    * IGDB-4031 Implementation of conditional display of oneAPI panels only during oneAPI debugging sessions (#153)
    
    * Added logic for hiding panels without content
    
    * Added ShowAll setting (#159)
    
    * Add dyn variable for colors (#158)
    
    * Fixed awaiting (#160)
    
    * IGDB-4113 Rework simd switch after GDB update (#161)
    
    * Changed getThread request
    
    * IGDB-4184 (#163)
    
    Switching to SIMD Lane 0 Does Not Update Context and "Selected Lane" Status
    Fixed+ Refresh added
    
    * Fixed Tooltip header (#164)
    
    * IGDB-4176 (#165)
    
    Implement frame-arguments none by default
    
    * chore: move few dependencies to devDependencies (#167)
    
    * Update Dependencies 2024.0
    
    * Update pathes
    
    * Update yarn.lock for dependabot
    
    ---------
    
    Co-authored-by: Schuchardt, Maciej <maciejx.schuchardt@intel.com>

--- src/viewProviders/selectedLaneViewProvider.ts
+++ src/viewProviders/selectedLaneViewProvider.ts
@@ -127,4 +127,17 @@ export class SelectedLaneViewProvider implements WebviewViewProvider {
             console.error("An error occurred while setting the view:", error);
         }
     }
+
+    // After setting "setContext", "oneapi:haveSIMD" to true, some time passes before initializing this._view,
+    // so we check its presence every 50 ms
+    private async ensureViewExists() {
+        return new Promise<void>((resolve) => {
+            const intervalId = setInterval(() => {
+                if (this._view && this._view.visible) {
+                    clearInterval(intervalId);
+                    resolve();
+                }
+            }, 50);
+        });
+    }
 }
