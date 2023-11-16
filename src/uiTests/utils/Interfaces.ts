9ed527ea7e5ef1fc5490bb3e354ad51d9e5d173b
Schuchardt, Maciej <maciejx.schuchardt@intel.com>
2023-11-16 13:22:32 +0000

    test: add RefreshSimdDataTest and ValidateOneApiGpuThreadsTest

---
+++ src/uiTests/utils/Interfaces.ts
@@ -0,0 +1,5 @@
+import { ICcppConfiguration } from "../models/ICcppConfiguration";
+import { INotification } from "../models/INotification";
+import { IVsCodeTask } from "../models/IVsCodeTask";
+
+export { ICcppConfiguration, INotification, IVsCodeTask };
