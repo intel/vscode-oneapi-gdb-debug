9f8d012b260ebc777243dbdeb9d03fd427a1743d
Maciej Schuchardt <maciejx.schuchardt@intel.com>
2024-01-23 14:04:07 +0100

    UI tests refactor (#180)
    Ui tests refactor

--- src/uiTests/models/index.ts
+++
@@ -1,8 +0,0 @@
-import { ICcppConfiguration } from "../models/ICcppConfiguration";
-import { INotification } from "../models/INotification";
-import { IVsCodeTask } from "../models/IVsCodeTask";
-import { IThread } from "../models/IThread";
-import { IBreakpoint } from "../models/IBreakpoint";
-import { IConditionalBreakpoint } from "../models/IConditionalBreakpoint";
-
-export { ICcppConfiguration, INotification, IVsCodeTask, IThread, IBreakpoint, IConditionalBreakpoint };
