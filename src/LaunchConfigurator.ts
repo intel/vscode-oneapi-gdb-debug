fb29c63a99306ea710f6cd5d04bf2d17b15f40fe
Sergey Bobko <sergey.bobko@intel.com>
2024-03-13 12:29:47 +0000

    Added gdb-oneai location notification

--- src/LaunchConfigurator.ts
+++ src/LaunchConfigurator.ts
@@ -7,7 +7,7 @@
 import * as vscode from "vscode";
 import { execSync } from "child_process";
 import { posix, parse } from "path";
- 
+
 const debugConfig = {
     comments: [
         "Full launch.json configuration details can be found here:",
@@ -37,41 +37,41 @@ const debugConfig = {
     MIMode: "gdb",
     miDebuggerPath: "gdb-oneapi",
     setupCommands:
-     [
-         {
-             description: "Enable pretty-printing for gdb",
-             text: "-enable-pretty-printing",
-             ignoreFailures: true
-         },
-         {
-             description: "Disable target async",
-             text: "set target-async off",
-             ignoreFailures: true
-         },
-         {
-             description: "Do not display function arguments when printing a stack frame",
-             text: "set print frame-arguments none",
-             ignoreFailures: true
-         }
-     ]
+        [
+            {
+                description: "Enable pretty-printing for gdb",
+                text: "-enable-pretty-printing",
+                ignoreFailures: true
+            },
+            {
+                description: "Disable target async",
+                text: "set target-async off",
+                ignoreFailures: true
+            },
+            {
+                description: "Do not display function arguments when printing a stack frame",
+                text: "set print frame-arguments none",
+                ignoreFailures: true
+            }
+        ]
 };
- 
+
 export class LaunchConfigurator {
- 
+
     async makeLaunchFile(): Promise<boolean> {
         if (process.platform === "win32") {
             vscode.window.showInformationMessage("This function cannot be used for Windows as a target platform. Generating configurations for debugging is only possible for use on Linux.", { modal: true });
             return false;
         }
         const workspaceFolder = await getworkspaceFolder();
- 
+
         if (!workspaceFolder) {
             return false; // for unit tests
         }
         const projectRootDir = `${workspaceFolder?.uri.fsPath}`;
         let execFiles: string[] = [];
         let execFile;
- 
+
         execFiles = await this.findExecutables(projectRootDir);
         execFiles.push("Leave it empty");
         execFiles.push("Provide path to the executable file manually");
@@ -79,10 +79,10 @@ export class LaunchConfigurator {
         const options: vscode.InputBoxOptions = {
             placeHolder: "Select the executable you want to debug. Press ESC to exit or if done creating debug configuration."
         };
- 
+
         do {
             let selection = await vscode.window.showQuickPick(execFiles, options);
- 
+
             if (!selection) {
                 isContinue = false;
                 break;
@@ -96,7 +96,7 @@ export class LaunchConfigurator {
                     canSelectMany: false
                 };
                 const pathToExecFile = await vscode.window.showOpenDialog(options);
- 
+
                 if (pathToExecFile && pathToExecFile[0]) {
                     execFile = pathToExecFile[0].fsPath;
                 } else {
@@ -106,19 +106,19 @@ export class LaunchConfigurator {
             } else {
                 execFile = selection;
             }
- 
+
             const stopAtEntrySelection = await vscode.window.showQuickPick(["yes", "no"], {
                 placeHolder: "Automatically break on main?"
             });
- 
+
             if (!stopAtEntrySelection) {
                 isContinue = false;
                 break;
             }
- 
+
             let argument: string | undefined;
             const args = [];
- 
+
             do {
                 argument = await vscode.window.showInputBox({
                     placeHolder: "Argument",
@@ -128,10 +128,10 @@ export class LaunchConfigurator {
                     args.push(argument);
                 }
             } while (argument?.trim().length);
- 
+
             const launchConfig = vscode.workspace.getConfiguration("launch");
             const configurations = launchConfig.configurations;
- 
+
             debugConfig.stopAtEntry = stopAtEntrySelection === "yes" ? true : false;
             debugConfig.args = [...args];
             debugConfig.name = selection === ""
@@ -140,7 +140,7 @@ export class LaunchConfigurator {
             debugConfig.program = `${execFile}`.split(/[\\/]/g).join(posix.sep);
             await this.addTasksToLaunchConfig();
             const isUniq: boolean = await this.checkLaunchItem(configurations, debugConfig);
- 
+
             if (isUniq) {
                 configurations.push(debugConfig);
                 launchConfig.update("configurations", configurations, false);
@@ -158,7 +158,7 @@ export class LaunchConfigurator {
             const no = "No";
             const selection = await vscode.window.showInformationMessage("Unable to identify oneAPI C++ launch configuration in your launch.json file.\
         Would you like to create a debug launch configuration now?", yes, no);
- 
+
             if (selection === yes) {
                 await vscode.commands.executeCommand("intelOneAPI.launchConfigurator.generateLaunchJson");
                 return;
@@ -168,11 +168,11 @@ export class LaunchConfigurator {
             }
         }
     }
- 
+
     async isThereDebugConfig(): Promise<boolean> {
         const launchConfig = vscode.workspace.getConfiguration("launch");
         const configs = launchConfig.configurations;
- 
+
         for (const cfg of configs) {
             if (cfg.type === "cppdbg" && cfg.miDebuggerPath === "gdb-oneapi") {
                 return true;
@@ -180,7 +180,7 @@ export class LaunchConfigurator {
         }
         return false;
     }
- 
+
     async checkGdb(): Promise<void> {
         if (!process.env.SETVARS_COMPLETED) {
             if (await this.checkEnvConfigurator()) {
@@ -188,8 +188,8 @@ export class LaunchConfigurator {
                 const custom_env = "custom environment using SETVARS_CONFIG";
                 const selection = await vscode.window.showInformationMessage("oneAPI environment is not configured.\
           Configure your development environment using \"Environment Configurator for Intel oneAPI Toolkits\".",
-                default_env, custom_env);
- 
+                    default_env, custom_env);
+
                 if (selection === default_env) {
                     await vscode.commands.executeCommand("intel-corporation.oneapi-environment-configurator.initializeEnvironment");
                 }
@@ -203,50 +203,70 @@ export class LaunchConfigurator {
          If you use setvars_config file make sure it includes a debugger");
         }
     }
- 
+
     private async checkEnvConfigurator(): Promise<boolean> {
         const tsExtension = vscode.extensions.getExtension("intel-corporation.oneapi-environment-configurator");
- 
+
         if (!tsExtension) {
             const GoToInstall = "Environment Configurator for Intel oneAPI Toolkits";
             const selection = await vscode.window.showInformationMessage("Please install the \"Environment Configurator for Intel oneAPI Toolkits\" to configure your development environment.", GoToInstall);
- 
+
             if (selection === GoToInstall) {
                 await vscode.commands.executeCommand("workbench.extensions.installExtension", "intel-corporation.oneapi-environment-configurator");
             }
             return false;
         }
         return true;
- 
+
     }
- 
+
     private isGdbInPath(): boolean {
-        if (process.env.PATH) {
-            const path = /oneAPI[\/\\]debugger[\/\\].+[\/\\]gdb[\/\\]intel64[\/\\]bin/gi;
-            const index = process.env.PATH.search(path);
- 
-            if (index !== -1) {
-                return true;
-            }
+        const paths = this.getGdbPaths();
+
+        if (paths.length === 0) {
+            vscode.window.showWarningMessage("gdb-oneapi not found.");
+            return false;
+        } else if (paths.length === 1) {
+            vscode.window.showInformationMessage(`gdb-oneapi found at: ${paths[0]}`);
+            return true;
+        } else {
+            vscode.window.showInformationMessage(`gdb-oneapi found at multiple locations:
+                                                    \n${paths.map((path, index) => `${index + 1}. ${path}`).join("\n")}
+                                                    \nThe first one will be used: ${paths[0]}`, { modal: true });
+            return true;
         }
-        return false;
     }
- 
+
+    private getGdbPaths(): string[] {
+        try {
+            const command = process.platform === "win32" ? "where" : "which -a ";
+            const result = execSync(`${command} gdb-oneapi`, { encoding: "utf8" });
+
+            // Split the result by newline to get an array of paths
+            const paths = result.split("\n");
+
+            // Remove any empty strings resulting from trailing newlines
+            return paths.filter(path => path);
+        } catch (error) {
+            return [];
+        }
+    }
+
     // eslint-disable-next-line  @typescript-eslint/no-explicit-any
     private async checkLaunchItem(listItems: { label: string }[], newItem: any): Promise<boolean> {
         if (listItems.length === 0) {
             return true; // for tests
         }
- 
+
         const existItem = listItems.find((item: { label: string }) => item.label === newItem.label);
         const dialogOptions: string[] = ["Cancel", "Rename configuration"];
- 
+
         if (existItem) {
             const options: vscode.InputBoxOptions = {
                 placeHolder: "A debug launch configuration already exists with this name. Do you want to rename this configuration or cancel?"
             };
             const selection = await vscode.window.showQuickPick(dialogOptions, options);
- 
+
             if (!selection || selection === "Cancel") {
                 return false;
             } else {
@@ -254,7 +274,7 @@ export class LaunchConfigurator {
                     placeHolder: "Please provide new configuration name:"
                 };
                 const inputName = await vscode.window.showInputBox(inputBoxText);
- 
+
                 if (!inputName) {
                     return false;
                 }
@@ -263,12 +283,12 @@ export class LaunchConfigurator {
         }
         return true;
     }
- 
+
     private async addTasksToLaunchConfig(): Promise<boolean> {
         const taskConfig = vscode.workspace.getConfiguration("tasks");
         const existTasks = taskConfig.tasks;
         const tasksList: string[] = [];
- 
+
         for (const task in existTasks) {
             tasksList.push(existTasks[task].label);
         }
@@ -277,7 +297,7 @@ export class LaunchConfigurator {
             placeHolder: "Choose a task to run before starting the debugger"
         };
         const preLaunchTask = await vscode.window.showQuickPick(tasksList, preLaunchTaskOptions);
- 
+
         if (preLaunchTask && preLaunchTask !== "Skip adding preLaunchTask") {
             debugConfig.preLaunchTask = preLaunchTask;
         }
@@ -285,25 +305,25 @@ export class LaunchConfigurator {
         const postDebugTaskOptions: vscode.InputBoxOptions = {
             placeHolder: "Choose a task to run after starting the debugger"
         };
- 
+
         tasksList.push("Skip adding postDebugTask");
         const postDebugTask = await vscode.window.showQuickPick(tasksList, postDebugTaskOptions);
- 
+
         if (postDebugTask && postDebugTask !== "Skip adding postDebugTask") {
             debugConfig.postDebugTask = postDebugTask;
         }
         return true;
     }
- 
+
     private async findExecutables(projectRootDir: string): Promise<string[]> {
         try {
             const cmd = process.platform === "win32"
                 ? `pwsh -command "Get-ChildItem '${projectRootDir}' -recurse -Depth 3 -include '*.exe' -Name | ForEach-Object -Process {$execPath='${projectRootDir}' +'\\'+ $_;echo $execPath}"`
                 : `find ${projectRootDir} -maxdepth 3 -exec file {} \\; | grep -i elf | cut -f1 -d ':'`;
             const pathsToExecutables = execSync(cmd).toString().split("\n");
- 
+
             pathsToExecutables.pop();
-            pathsToExecutables.forEach(async function(onePath, index, execList) {
+            pathsToExecutables.forEach(async function (onePath, index, execList) {
                 // This is the only known way to replace \\ with /
                 execList[index] = posix.normalize(onePath.replace("\r", "")).split(/[\\/]/g).join(posix.sep);
             });
@@ -314,13 +334,13 @@ export class LaunchConfigurator {
         }
     }
 }
- 
+
 async function getworkspaceFolder(): Promise<vscode.WorkspaceFolder | undefined> {
     if (vscode.workspace.workspaceFolders?.length === 1) {
         return vscode.workspace.workspaceFolders[0];
     }
     const selection = await vscode.window.showWorkspaceFolderPick();
- 
+
     if (!selection) {
         vscode.window.showErrorMessage("Cannot find the working directory.", { modal: true });
         vscode.window.showInformationMessage("Please add one or more working directories and try again.");
@@ -328,4 +348,3 @@ async function getworkspaceFolder(): Promise<vscode.WorkspaceFolder | undefined>
     }
     return selection;
 }
- 
