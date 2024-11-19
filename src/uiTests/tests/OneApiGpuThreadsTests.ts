05af9826e583332d71a69d89c69f110114e2a0e7
Schuchardt, Maciej <maciej.schuchardt@intel.com>
2024-11-19 07:28:26 +0000

    tests: add "Check if SIMD lane symbols are displayed" test

--- src/uiTests/tests/OneApiGpuThreadsTests.ts
+++ src/uiTests/tests/OneApiGpuThreadsTests.ts
@@ -4,12 +4,14 @@
  */
 
 import { RemoveAllBreakpoints, SetConditionalBreakpoint, ContinueDebugging, CheckIfBreakpointConditionHasBeenMet } from "../utils/Debugging/Debugging";
+import { GetRandomInt, LaunchSequence, ReplaceStringInFile, SetSettingValue, Wait } from "../utils/CommonFunctions";
 import { GetGpuThreads, GetCurrentThread } from "../utils/OneApiGpuThreads/OneApiGpuThreads";
 import { ConditionalBreakpoint, ConditionalBreakpointType } from "../utils/Debugging/Types";
-import { GetRandomInt, LaunchSequence, Wait } from "../utils/CommonFunctions";
+import { SimdLane, Thread } from "../utils/OneApiGpuThreads/Types";
 import { LoggerAggregator as logger } from "../utils/Logger";
-import { Thread } from "../utils/OneApiGpuThreads/Types";
+import { By, Workbench } from "vscode-extension-tester";
 import { assert } from "chai";
+import { FileExistsSync } from "../utils/FileSystem";
 
 export default function() {
     describe("Validate 'OneAPI GPU Threads' pane", () => {
@@ -23,6 +25,11 @@ export default function() {
             this.timeout(10 * this.test?.ctx?.defaultTimeout);
             this.retries(1);
             await CheckCurrentLaneIndicatorTest();
+        });
+        it("Check SIMD lanes symbols", async function() {
+            this.timeout(4 * this.test?.ctx?.defaultTimeout);
+            this.retries(1);
+            await CheckSimdLaneSymbols(this.resources);
         });
     });
 }
@@ -109,7 +116,73 @@ async function CheckCurrentLaneIndicatorTest(): Promise<void> {
         logger.Error(e);
         throw e;
     }
+}
 
+async function CheckSimdLaneSymbols(resources: string[]): Promise<void> {
+    logger.Info("Check if SIMD lane symbols are displayed");
+    const indicator = "🠶";
+    const activeLaneSymbol = "A";
+    const inactiveLaneSymbol = "I";
+    const path = GetArrayTransformFilePath(resources) as string;
+
+    try {
+
+        // Change array length to '65' to spawn inactive lanes "constexpr size_t length = .*"
+        ReplaceStringInFile("constexpr size_t length", "constexpr size_t length = 65;", path);
+        await LaunchSequence();
+        await Wait(3 * 1000);
+        const allThreads = await GetGpuThreads();
+        const currentThread = await GetCurrentThread(allThreads) as Thread;
+        const currentLane = currentThread.simdLanes.find(s => s.current) as SimdLane;
+
+        logger.Info(`Check if current lane indicator '${indicator}' is present on current lane '${currentLane.laneId}'`);
+        assert.strictEqual(currentLane.indicator, indicator, `Current lane indicator '${indicator}' is not present on current lane '${currentLane.laneId}'`);
+        logger.Info(`Current lane indicator '${indicator}' is present on current lane '${currentLane.laneId}'`);
+        const settingsEditor = await new Workbench().openSettings();
+
+        await SetSettingValue("ACTIVE_LANE_SYMBOL", "Aa", settingsEditor);
+        const activeLanevalidationMessage = await settingsEditor.getDriver().findElement(By.className("setting-item-validation-message")).getText();
+
+        logger.Info("Check if validation message is 'Value must be 1 or fewer characters long.'");
+        assert.strictEqual(activeLanevalidationMessage, "Value must be 1 or fewer characters long.", "Validation message is not equal to 'Value must be 1 or fewer characters long.'");
+        logger.Pass("Validation message is 'Value must be 1 or fewer characters long.'");
+        await SetSettingValue("ACTIVE_LANE_SYMBOL", activeLaneSymbol, settingsEditor);
+        await SetSettingValue("INACTIVE_LANE_SYMBOL", "Ii", settingsEditor);
+        const inactiveLanevalidationMessage = await settingsEditor.getDriver().findElement(By.className("setting-item-validation-message")).getText();
+
+        logger.Info("Check if validation message is 'Value must be 1 or fewer characters long.'");
+        assert.strictEqual(inactiveLanevalidationMessage, "Value must be 1 or fewer characters long.", "Validation message is not equal to 'Value must be 1 or fewer characters long.'");
+        logger.Pass("Validation message is 'Value must be 1 or fewer characters long.'");
+        await SetSettingValue("INACTIVE_LANE_SYMBOL", inactiveLaneSymbol, settingsEditor);
+        const threadsAfterSettingsChange = await GetGpuThreads();
+        const allActiveLanes = threadsAfterSettingsChange.flatMap(t => t.simdLanes.filter(l => l.state === "Active" || l.state === "Hit"));
+        const allInactiveLanes = threadsAfterSettingsChange.flatMap(t => t.simdLanes.filter(l => l.state === "Inactive"));
+
+        logger.Info(`Check if all active and hit lanes contains custom lane symbol '${activeLaneSymbol}'`);
+        assert.isTrue(allActiveLanes.every(l => l.current ? true : l.customSymbol === activeLaneSymbol), `Not all active lanes has custom lane symbol '${activeLaneSymbol}'`);
+        logger.Pass(`All active and hit lanes contains custom lane symbol '${activeLaneSymbol}'`);
+
+        logger.Info(`Check if all inactive lanes contains custom lane sumbol '${inactiveLaneSymbol}'`);
+        assert.isTrue(allInactiveLanes.every(l => l.customSymbol === inactiveLaneSymbol), `Not all inactive lanes has custom lane symbol '${inactiveLaneSymbol}'`);
+        logger.Pass(`All inactive lanes contains custom lane sumbol '${inactiveLaneSymbol}'`);
+    } catch (e) {
+        logger.Error(e);
+        throw e;
+    } finally {
+        ReplaceStringInFile("constexpr size_t length", "constexpr size_t length = 64;", path);
+    }
 }
 
 //#endregion
+
+function GetArrayTransformFilePath(resources: string[]): string | undefined {
+    const test_resources = process.env["TEST_RESOURCES"];
+    let arrayTransformPath = test_resources?.includes("array-transform") ? test_resources.concat("/src/array-transform.cpp") : undefined;
+
+    arrayTransformPath = arrayTransformPath ?? resources.find(x => x.includes(".cpp"));
+    if (!FileExistsSync(arrayTransformPath ?? "")) {
+        return;
+    }
+
+    return arrayTransformPath;
+}
