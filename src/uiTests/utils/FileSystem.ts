058dac638ee6a486ac71b15d205ff01f8707e57c
Schuchardt, Maciej <maciej.schuchardt@intel.com>
2024-12-09 12:59:21 +0100

    refactor: adjust FileSystem

--- src/uiTests/utils/FileSystem.ts
+++ src/uiTests/utils/FileSystem.ts
@@ -3,73 +3,116 @@
  * SPDX-License-Identifier: MIT
  */
 
-import { PathLike, PathOrFileDescriptor, existsSync, mkdirSync, readFileSync, rmSync, writeFileSync } from "fs";
+import { existsSync, mkdirSync, PathLike, PathOrFileDescriptor, readFileSync, rmSync, writeFileSync } from "fs";
 import { LoggerAggregator as logger } from "./Logger";
+import { FsOptions } from "./Types";
+import { NodeSSH } from "node-ssh";
 
-/**
- * Checks if the given `path` exists.
- * @param path Path to look for.
- * @returns Returns `true` if the path exists, `false` otherwise.
- */
-export function FileExistsSync(path: PathLike): boolean {
-    logger.Info(`Check if given path '${path}' exists`);
-    const exists = existsSync(path);
+export abstract class FileSystem {
+    private static sshClient: NodeSSH | undefined;
 
-    logger.Info(`Path '${path}'${exists ? "" : " does not"} exists`);
-    return exists;
-}
+    public static Init(sshClient: NodeSSH) {
+        if (!FileSystem.sshClient) {
+            FileSystem.sshClient = sshClient;
+        }
+    }
 
-/**
- * Writes given `data` to the `file`.
- * @param file Path to the file.
- * @param data Data to write to the given `file`.
- */
-export function WriteFileSync(file: PathOrFileDescriptor, data: string | NodeJS.ArrayBufferView): void {
-    logger.Info(`Write '${file}' file`);
-    writeFileSync(file, data);
-    logger.Info(`File '${file}' has been written`);
-}
+    /**
+     * Checks if the given `path` exists.
+     * @param path Path to look for.
+     * @returns Returns `true` if the path exists, `false` otherwise.
+     */
+    static async FileExistsAsync(path: PathLike, options: FsOptions): Promise<boolean> {
+        logger.Info(`Check if given path '${path}' exists`);
+        return await FileSystem.Exec(async ssh => {
+            const response = await ssh.execCommand(`test -f ${path}`);
 
-/**
- * Synchronously reads the entire contents of a file.
- * @param path A path to a file.
- * @param encoding File encoding.
- * @returns File contents as `string`.
- */
-export function ReadFileSync(path: PathOrFileDescriptor, encoding: BufferEncoding): string {
-    logger.Info(`Read '${path}' file`);
-    const file = readFileSync(path, encoding);
+            return response.code === 0;
+        }, async() => {
+            return existsSync(path);
+        }, options);
+    }
 
-    logger.Info(`File '${path}' has been read`);
-    return file;
-}
+    /**
+     * Writes given `data` to the `file`.
+     * @param file Path to the file.
+     * @param data Data to write to the given `file`.
+     */
+    static async WriteFileAsync(file: PathOrFileDescriptor, data: string | NodeJS.ArrayBufferView, options: FsOptions): Promise<void> {
+        logger.Info(`Write '${file}' file`);
+        await FileSystem.Exec(async ssh => {
+            await ssh.execCommand(`echo \'${data}\' > ${file}`);
+        }, async() => {
+            writeFileSync(file, data);
+        }, options);
+        logger.Info(`File '${file}' has been written`);
+    }
 
-/**
- * 
- * @param path A path to a file. If a URL is provided, it must use the file: protocol.
- */
-export function MkdirSync(path: PathLike): void {
-    logger.Info(`Create '${path}' directory`);
-    mkdirSync(path);
-    logger.Info(`Directory '${path}' has been created`);
-}
+    /**
+     * Synchronously reads the entire contents of a file.
+     * @param path A path to a file.
+     * @param encoding File encoding.
+     * @returns File contents as `string`.
+     */
+    static async ReadFileAsync(path: PathOrFileDescriptor, encoding: BufferEncoding, options: FsOptions): Promise<string> {
+        logger.Info(`Read '${path}' file`);
+        return await FileSystem.Exec(async ssh => {
+            const response = await ssh.execCommand(`cat ${path}`, {
+                onStdout: buffer => buffer.toString(encoding)
+            });
 
-/**
- * Loads and parses given JSON file.
- */
-export function LoadAndParseJsonFile<T>(path: PathOrFileDescriptor): T {
-    logger.Info(`Load '${path}' file`);
-    const file = JSON.parse(ReadFileSync(path, "utf-8")) as T;
+            return response.stdout;
+        }, async() => {
+            const file = readFileSync(path, encoding);
 
-    return file;
-}
+            return file;
+        }, options);
+    }
 
-/**
- * Removes file or dir at given path.
- * @param path Path to a file or dir to remove.
- */
-export function RmSync(path: PathLike): void {
-    logger.Info(`Remove '${path}' directory`);
-    rmSync(path, { recursive: true, force: true });
-    logger.Info(`Directory '${path}' has been removed`);
+    /**
+     * 
+     * @param path A path to a file. If a URL is provided, it must use the file: protocol.
+     */
+    static async MkdirAsync(path: PathLike, options: FsOptions): Promise<void> {
+        logger.Info(`Create '${path}' directory`);
+        await FileSystem.Exec(async(ssh) => {
+            await ssh.execCommand(`mkdir ${path}`);
+        }, async() => {
+            mkdirSync(path);
+        }, options);
+        logger.Info(`Directory '${path}' has been created`);
+    }
+
+    /**
+     * Loads and parses given JSON file.
+     */
+    static async LoadAndParseJsonFile<T>(path: PathOrFileDescriptor, options: FsOptions): Promise<T> {
+        logger.Info(`Load '${path}' file`);
+        const file = await FileSystem.ReadFileAsync(path, "utf-8", options);
+
+        return JSON.parse(file) as T;
+    }
+
+    /**
+     * Removes file or dir at given path.
+     * @param path Path to a file or dir to remove.
+     */
+    static async RmAsync(path: string, options: FsOptions): Promise<void> {
+        logger.Info(`Remove '${path}' directory`);
+        await FileSystem.Exec(async(ssh) => {
+            await ssh.execCommand(`rm -rf ${path}`);
+        }, async() => {
+            rmSync(path, { recursive: true, force: true });
+        }, options);
+        logger.Info(`Directory '${path}' has been removed`);
+    }
+
+    private static async Exec<T>(remote: (client: NodeSSH) => Promise<T>, local: () => Promise<T>, options: FsOptions): Promise<T> {
+        if (options?.remotePath) {
+            if (!FileSystem.sshClient) { throw new Error("remotePath was set to true but sshClient is undefined!"); }
+            return await remote(FileSystem.sshClient);
+        } else {
+            return await local();
+        }
+    }
 }
