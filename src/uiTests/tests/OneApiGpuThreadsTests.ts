97f8caf4028f3960f0b1bcec6e5c790c1cbd3c73
Schuchardt, Maciej <maciej.schuchardt@intel.com>
2025-04-04 12:48:13 +0000

    refactor: add more log messages

--- src/uiTests/tests/OneApiGpuThreadsTests.ts
+++ src/uiTests/tests/OneApiGpuThreadsTests.ts
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: MIT
  */
 
-import { LaunchSequence, Retry, SetInputText, Wait } from "../utils/CommonFunctions";
+import { LaunchSequence, Wait } from "../utils/CommonFunctions";
 import { LoggerAggregator as logger } from "../utils/Logger";
 import { GetGpuThreads } from "../utils/OneApiGpuThreads";
 import { assert } from "chai";
@@ -11,7 +11,7 @@ import { assert } from "chai";
 export default function() {
     describe("Validate 'OneAPI GPU Threads' pane", () => {
         it("Check if 'TargetID' is available", async function() {
-            this.timeout(this.test?.ctx?.defaultTimeout * 120);
+            this.timeout(this.test?.ctx?.defaultTimeout * 3); // Three minutes timeout.
             this.retries(1);
             await CheckTargetIdTest();
         });
@@ -23,14 +23,17 @@ export default function() {
 async function CheckTargetIdTest(): Promise<void> {
     logger.Info("Check 'TargetID' Test");
     try {
-    await LaunchSequence();
-    await Wait(3 * 1000);
+        await LaunchSequence();
+        await Wait(3 * 1000);
 
-    const gpuThreads = await GetGpuThreads();
-    const targetIds = gpuThreads.map(x => x.targetId);
-    const result = targetIds.every(x => (/^ZE \d+.\d+.\d+.\d+$/.test(x)));
-    assert.isTrue(result, `'TargetID's are not valid. Actual:\n${targetIds.join("\n")}\nExpected: Satisfy the pattern "/ZE \d+.\d+.\d+.\d+/" eg. "ZE 0.1.3.0"`);
-    } catch(e) {
+        const gpuThreads = await GetGpuThreads();
+        const targetIds = gpuThreads.map(x => x.targetId);
+        const result = targetIds.every(x => (/^ZE \d+.\d+.\d+.\d+$/.test(x)));
+        const message = `Actual:\n${targetIds.join("\n")}`;
+        logger.Info(`Check if 'TargetID's are available. ${message}`)
+        assert.isTrue(result, `'TargetID's are not valid. ${message}\nExpected: Satisfy the pattern "/ZE \d+.\d+.\d+.\d+/" eg. "ZE 0.1.3.0"`);
+        logger.Pass("'TargetID's are available and valid!")
+    } catch (e) {
         logger.Error(e);
         throw e;
     }
